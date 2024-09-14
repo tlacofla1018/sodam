9f3fdf070bddb330b6f7b678b0bc728f74e75e9c
tlacofla1018 <chaerm99@gmail.com>
2024-09-14 23:01:05 +0900

    date: 0914 작업기록

---
+++ html/js/main.js
@@ -0,0 +1,31 @@
+// mainBanner Swiper
+var swiper01 = new Swiper("#mainBanner.swiper.mySwiper", {});
+
+// movieRank01 Swiper
+let appendNumber02 = 600;
+let prependNumber02 = 1;
+const swiper02 = new Swiper('#nowMovieRankContents01.swiper', {
+    slidesPerView: 5,
+    centeredSlides: false,
+    spaceBetween: 20,
+    pagination: {
+    el: '#nowMovieRankContents01 .swiper-pagination',
+    type: 'fraction',
+    },
+    navigation: {
+        nextEl: '#nowMovieRankContents01 .swiper-button-next',
+        prevEl: '#nowMovieRankContents01 .swiper-button-prev',
+    },
+    virtual: {
+        slides: (function () {
+            const slides = [];
+            for (var i = 0; i < 600; i += 1) {
+                slides.push('Slide ' + (i + 1));
+            }
+            return slides;
+        })(),
+    },
+});
+
+// middleBanner Swiper
+var swiper03 = new Swiper("#middleBanner.swiper.mySwiper", {});
